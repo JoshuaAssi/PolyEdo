f44bc209f866756df8d2195a85feb8c782bac0b2
legster <66693306+legster@users.noreply.github.com>
2020-06-24 16:42:49 -0400

    Update modals.js

--- modals.js
+++ modals.js
@@ -1,30 +1,30 @@
     
     // Get the modal
-    var modal1 = document.getElementById("modal1");
-    var modal2 = document.getElementById("modal2");
-    var modal3 = document.getElementById("modal3");
-    var modal4 = document.getElementById("modal4");
-    var modal5 = document.getElementById("modal5");
-    var modal6 = document.getElementById("modal6");
-    var shmodal = document.getElementById("shmodal");
+    let modal1 = document.getElementById("modal1");
+    let modal2 = document.getElementById("modal2");
+    let modal3 = document.getElementById("modal3");
+    let modal4 = document.getElementById("modal4");
+    let modal5 = document.getElementById("modal5");
+    let modal6 = document.getElementById("modal6");
+    let shmodal = document.getElementById("shmodal");
 
     // Get the button that opens the modal
-    var btn1 = document.getElementById("him1");
-    var btn2 = document.getElementById("him2");
-    var btn3 = document.getElementById("him3");
-    var btn4 = document.getElementById("him4");
-    var btn5 = document.getElementById("him5");
-    var btn6 = document.getElementById("him6");
-    var shbtn = document.getElementById("shcart");
+    let btn1 = document.getElementById("him1");
+    let btn2 = document.getElementById("him2");
+    let btn3 = document.getElementById("him3");
+    let btn4 = document.getElementById("him4");
+    let btn5 = document.getElementById("him5");
+    let btn6 = document.getElementById("him6");
+    let shbtn = document.getElementById("shcart");
     
     // Get the Xs
-    /*var closer1 = document.getElementsByClassName("close")[0];
-    var closer2 = document.getElementsByClassName("close")[1];
-    var closer3 = document.getElementsByClassName("close")[2];
-    var closer4 = document.getElementsByClassName("close")[3];
-    var closer5 = document.getElementsByClassName("close")[4];
-    var closer6 = document.getElementsByClassName("close")[5];
-    var shcloser = document.getElementsByClassName("close")[6]; */
+    /*let closer1 = document.getElementsByClassName("close")[0];
+    let closer2 = document.getElementsByClassName("close")[1];
+    let closer3 = document.getElementsByClassName("close")[2];
+    let closer4 = document.getElementsByClassName("close")[3];
+    let closer5 = document.getElementsByClassName("close")[4];
+    let closer6 = document.getElementsByClassName("close")[5];
+    let shcloser = document.getElementsByClassName("close")[6]; */
 
     // When the user clicks the button, open the modal 
     btn1.onclick = function() {
@@ -100,15 +100,15 @@ shcloser.onclick = function() {
     }
 
 //sizebuttons
-var wvsb = document.getElementById("wvsbutton");
-var wvmb = document.getElementById("wvmbutton");
-var wvlb = document.getElementById("wvlbutton");
-var wvxlb = document.getElementById("wvxlbutton");
+let wvsb = document.getElementById("wvsbutton");
+let wvmb = document.getElementById("wvmbutton");
+let wvlb = document.getElementById("wvlbutton");
+let wvxlb = document.getElementById("wvxlbutton");
 
-var wvsval = 0;
-var wvmval = 0;
-var wvlval = 0;
-var wvxlval = 0;
+let wvsval = 0;
+let wvmval = 0;
+let wvlval = 0;
+let wvxlval = 0;
 
 
 //size buttons color and value changes
@@ -154,32 +154,42 @@ wvxlbutton.onclick = function(event) {
     wvxlval = 1;
 }
 
-var wvqupb = document.getElementById("wvqupb");
-var wvqdownb = document.getElementById("wvqdownb");
-var wvqnum = document.getElementById("wvqnum");
-var nothing = document.getElementById("nothing");
-var wvqval = 1; //underneath quantity, qnum is display
+let wvqupb = document.getElementById("wvqupb");
+let wvqdownb = document.getElementById("wvqdownb");
+let wvqnum = document.getElementById("wvqnum");
+let nothing = document.getElementById("nothing");
+let wvqval = 1; //underneath quantity, qnum is display
 
-var $q1 = 1; //final q of item 1
-var $size1; //final size of i1
-var $item1 = 0; //1: item in slot, 0: none
+let wvprice = 20.00; //price of wv item
 
-var $q2 = 1;
-var $size2;
-var $item2 = 0;
+let $q1 = 1; //final q of item 1
+let $size1; //final size of i1
+let $item1 = 0; //1: item in slot, 0: none
+let $price1 = 0; //final price of i1
 
-var $q3 = 1;
-var $size3;
-var $item3 = 0;
+let $q2 = 1;
+let $size2;
+let $item2 = 0;
+let $price2 = 0;
 
-var $q4 = 1;
-var $size4;
-var $item4 = 0;
+let $q3 = 1;
+let $size3;
+let $item3 = 0;
+let $price3 = 0;
 
-var $q5 = 1;
-var $size5;
-var $item5 = 0;
+let $q4 = 1;
+let $size4;
+let $item4 = 0;
+let $price4 = 0;
 
+let $q5 = 1;
+let $size5;
+let $item5 = 0;
+let $price5 = 0;
+
+let $totalitems = document.getElementById("totalitems");
+let $totalprice = document.getElementById("totalprice");
+let $intprice = 0;
 
 //up button upincs q if q<20, down downincs q if q>1
 wvqupb.onclick = function(event) {
@@ -197,7 +207,7 @@ wvqdownb.onclick = function(event) {
 
 
 //when wv cart is clicked:
-var wvshcart = document.getElementById("wvshcart");
+let wvshcart = document.getElementById("wvshcart");
 wvshcart.onclick = function() {
   if ($item1 + $item2 + $item3 + $item4 + $item5 == 5) { //all occupied
     alert("Your Cart is Full");
@@ -248,6 +258,8 @@ wvshcart.onclick = function() {
       document.getElementById("modal1").style="display:none";
       $item5 = 1;
       }
+      $price5 = $q5 * wvprice;
+      document.getElementById("$price5").innerHTML = "$" + $price5;
     }
   }
   if ($item1 + $item2 + $item3 == 3) { //i1, i2, and i3 are occupied
@@ -296,6 +308,8 @@ wvshcart.onclick = function() {
       document.getElementById("modal1").style="display:none";
       $item4 = 1;
       }
+      $price4 = $q4 * wvprice;
+      document.getElementById("$price4").innerHTML = "$" + $price4;
     }
   }
   if ($item1 + $item2 == 2) { //i1 and i2 are occupied
@@ -344,6 +358,8 @@ wvshcart.onclick = function() {
       document.getElementById("modal1").style="display:none";
       $item3 = 1;
       }
+      $price3 = $q3 * wvprice;
+      document.getElementById("$price3").innerHTML = "$" + $price3;
     }
   }
   if ($item1 == 1) { //i1 is occupied
@@ -392,10 +408,12 @@ wvshcart.onclick = function() {
       document.getElementById("modal1").style="display:none";
       $item2 = 1;
       }
+      $price2 = $q2 * wvprice;
+      document.getElementById("$price2").innerHTML = "$" + $price2;
     }
   }
   if ($item1 == 0) { //i1 is available
-  if (wvsval == 1) { //add wv values to i1
+    if (wvsval == 1) { //add wv values to i1
     $q1 = wvqval;
     $size1 = "Small";
     document.getElementById("nothing").style="display:none";
@@ -439,52 +457,93 @@ wvshcart.onclick = function() {
   document.getElementById("modal1").style="display:none";
   $item1 = 1; 
   }
+  $price1 = $q1 * wvprice;
+      document.getElementById("$price1").innerHTML = "$" + $price1;
   }
   if (wvsval + wvmval + wvlval + wvxlval == 0) { //if no size is selected: alert
     alert("Please select a size");
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }
 
-var cancel1 = document.getElementById("cancel1");
-var cancel2 = document.getElementById("cancel2");
-var cancel3 = document.getElementById("cancel3");
-var cancel4 = document.getElementById("cancel4");
-var cancel5 = document.getElementById("cancel5");
-var checkbar = document.getElementById("checkbar");
+let cancel1 = document.getElementById("cancel1");
+let cancel2 = document.getElementById("cancel2");
+let cancel3 = document.getElementById("cancel3");
+let cancel4 = document.getElementById("cancel4");
+let cancel5 = document.getElementById("cancel5");
+let checkbar = document.getElementById("checkbar");
 
 //cancel shopping cart items
 cancel1.onclick = function () {
   $item1 =0;
   shbox1.style ="display: none;";
-  if ($item1 + $item2 + $item3 + $item4 == 0) {
+  if ($item1 + $item2 + $item3 + $item4 + $item5 == 0) {
     nothing.style="display: block;";
     checkbar.style="display: none";
-
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }
 cancel2.onclick = function () {
   $item2 =0;
   shbox2.style ="display: none;";
-  if ($item1 + $item2 + $item3 + $item4 == 0) {
+  if ($item1 + $item2 + $item3 + $item4 + $item5== 0) {
     nothing.style="display: block;";
     checkbar.style="display: none";
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }
 cancel3.onclick = function () {
   $item3 = 0;
   shbox3.style ="display: none;";
-  if ($item1 + $item2 + $item3 + $item4 == 0) {
+  if ($item1 + $item2 + $item3 + $item4 + $item5 == 0) {
     nothing.style="display: block;";
     checkbar.style="display: none";
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }
 cancel4.onclick = function () {
   $item4 =0;
   shbox4.style ="display: none;";
-  if ($item1 + $item2 + $item3 + $item4 == 0) {
+  if ($item1 + $item2 + $item3 + $item4 + $item5 == 0) {
     nothing.style="display: block;";
     checkbar.style="display: none";
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }
 cancel5.onclick = function () {
   $item5 =0;
@@ -493,4 +552,12 @@ cancel5.onclick = function () {
     nothing.style="display: block;";
     checkbar.style="display: none";
   }
+  if ($q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 == 1) { //qs only added if their corresponding i is occupied, rest terms == 0 and are not counted
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " item";
+  }
+  else {
+    $totalitems.innerHTML = $q1 * $item1 + $q2 * $item2 + $q3 * $item3 + $q4 * $item4 + $q5 * $item5 + " items";
+  }
+  $intprice = $price1 * $item1 + $price2 * $item2 + $price3 * $item3 + $price4 * $item4 + $price5 * $item5;
+  $totalprice.innerHTML = "Total: $" + $intprice;
 }

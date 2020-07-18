fe1a3c6426bfb0363e28a1a3dfc8df3fcbd931b7
legster <66693306+legster@users.noreply.github.com>
2020-07-18 12:21:28 -0400

    Add files via upload

---
+++ opener.js
@@ -0,0 +1,23 @@
+
+function betweener(){
+    setTimeout(function(){$('#edo').animate({opacity:"1",}, 100, after2wait())}, 300)
+}
+
+function sendoff() {
+    window.location.replace("index.html")
+}
+function after2wait() {
+    setTimeout(function(){$('#edo').animate({opacity: "0",}, 250, sendoff());}, 600)
+}
+function after1wait() {
+    setTimeout(function(){$('#poly').animate({opacity:"0",}, 100, betweener());}, 600)
+}
+
+function openfunc(){
+    setTimeout(function(){
+    $('#poly').animate({
+        opacity: "1",
+    }, 100, after1wait()
+    )
+},500)
+}; 
